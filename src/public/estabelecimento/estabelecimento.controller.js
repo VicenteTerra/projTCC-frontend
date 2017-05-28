dbb7c75eba58b3d92a34bedae9b9fe8b6a5c0de2
Vicente Terra <vicenteterra@gmail.com>
2017-05-27 23:24:21 -0300

    consulta de alunos implementada parcialmente + testes de layouts

--- src/public/estabelecimento/estabelecimento.controller.js
+++ src/public/estabelecimento/estabelecimento.controller.js
@@ -3,19 +3,37 @@
   angular.module('public')
   .controller('estabelecimentoController',estabelecimentoController);
 
-  estabelecimentoController.$inject = ['alunoService' ];
-  function estabelecimentoController(alunoService) {
+  estabelecimentoController.$inject = ['alunoService' ,'$scope', '$mdSidenav'];
+  function estabelecimentoController(alunoService, $scope , $mdSidenav) {
     var vm = this;
     vm.consultaCpf = "";
     vm.usuarioConsulta = {};
-    vm.showImg = false;
-
+    vm.showResultConsulta = false;
+    vm.toggle = true;
 
     vm.consultar = function () {
-    alunoService.getAlunoByCpf(vm.consultaCpf).then(function (response) {
-      vm.usuarioConsulta = response;
-      vm.showImg = true;
-    });
+      alunoService.getAlunoByCpf(vm.consultaCpf).then(function (response) {
+        if(response.status == 0){
+          vm.usuarioConsulta = response.alunoConsulta;
+          vm.showResultConsulta = true;
+        }else{
+          var notify = {
+            type: 'error',
+            title: response.message,
+            content: '',
+            timeout: 5000 
+          };
+          $scope.$emit('notify', notify);
+        }
+      });
+    }
+    vm.limparConsulta = function(){
+      vm.showResultConsulta = false;
+      vm.consultaCpf = "";
+    }
+
+    vm.toggleSideNav = function(){
+      vm.toggle = !vm.toggle;
     }
 
 
